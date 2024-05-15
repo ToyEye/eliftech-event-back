99293e147846ab22026b3f149a2d2b0ae4ae3a07
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-15 19:28:23 +0300

    change

--- server.js
+++ server.js
@@ -5,14 +5,30 @@ import app from "./app.js";
 const MONGO_DB = process.env.MONGO_DB;
 const PORT = process.env.PORT || 4000;
 
-mongoose
-  .connect(MONGO_DB)
-  .then(() => {
-    console.log("Mongo on");
-    app.listen(PORT, () => {
-      console.log("server on");
-    });
-  })
-  .catch(() => {
+// mongoose
+//   .connect(MONGO_DB)
+//   .then(() => {
+//     console.log("Mongo on");
+//     app.listen(PORT, () => {
+//       console.log("server on");
+//     });
+//   })
+//   .catch(() => {
+//     process.exit(1);
+//   });
+
+mongoose.set("strictQuery", false);
+const startServer = async () => {
+  try {
+    await mongoose.connect(MONGO_DB);
+
+    app.listen(PORT, () =>
+      console.log(`Database connection successful ${PORT}`)
+    );
+  } catch (e) {
+    console.log(e.message);
     process.exit(1);
-  });
+  }
+};
+
+startServer();
