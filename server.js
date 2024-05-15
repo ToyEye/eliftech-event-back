8d69508667ef16755ce6245139d1b5c05f15b37b
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-15 17:27:23 +0300

    add

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 import app from "./app.js";
 
-const { MONGO_DB } = process.env;
+const MONGO_DB = process.env.MONGO_DB;
+const PORT = process.env.PORT || 4000;
 
 mongoose
   .connect(MONGO_DB)
   .then(() => {
     console.log("Mongo on");
-    app.listen(3001, () => {
+    app.listen(PORT, () => {
       console.log("server on");
     });
   })
