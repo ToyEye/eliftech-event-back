e1c32b56166d533583a73c3c37f8f7dd71b96117
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-15 19:55:54 +0300

    fix

--- server.js
+++ server.js
@@ -5,30 +5,14 @@ import app from "./app.js";
 const MONGO_DB = process.env.MONGO_DB;
 const PORT = process.env.PORT || 4000;
 
-// mongoose
-//   .connect(MONGO_DB)
-//   .then(() => {
-//     console.log("Mongo on");
-//     app.listen(PORT, () => {
-//       console.log("server on");
-//     });
-//   })
-//   .catch(() => {
-//     process.exit(1);
-//   });
-
-mongoose.set("strictQuery", false);
-const startServer = async () => {
-  try {
-    await mongoose.connect(MONGO_DB);
-
-    app.listen(PORT, () =>
-      console.log(`Database connection successful ${PORT}`)
-    );
-  } catch (e) {
-    console.log(e.message);
+mongoose
+  .connect(MONGO_DB)
+  .then(() => {
+    console.log("Mongo on");
+    app.listen(PORT, () => {
+      console.log("server on");
+    });
+  })
+  .catch(() => {
     process.exit(1);
-  }
-};
-
-startServer();
+  });
